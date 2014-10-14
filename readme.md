9e0ec0f91051f7142fb6d6cef826b2496a992ad7
Jack Moore <hello@jacklmoore.com>
2014-10-14 08:42:02 -0400

    Removed jQuery dependency Added 'wheelzoom.destroy' event for removing changes that wheelzoom has made to an element.

--- readme.md
+++ readme.md
@@ -1,22 +1,26 @@
-# jQuery Wheelzoom
+# Wheelzoom
 
-A small plugin for zooming IMG elements with the mousewheel/trackpad.  Wheelzoom works by scaling and positioning a background-image, after it sets the IMG element's src to a transparent png.  Wheelzoom doesn't add any extra elements to the DOM, or change the positioning of the IMG element.
+A small script for zooming IMG elements with the mousewheel/trackpad.  Wheelzoom works by replacing the img element's src with a transparent image, then using the original src as a background image, which can be sized and positioned. Wheelzoom is dependency free, doesn't add any extra elements to the DOM, or change the positioning of the IMG element.
 
 ## Usage:
 ````javascript
-$('img').wheelzoom();
+wheelzoom(document.querySelectorAll('img'));
 // or
-$('img').wheelzoom({zoom:0.05});
+wheelzoom(document.querySelectorAll('img'), {zoom:0.05});
 // zoom sets the zoom percent.
 ````
 
 ## License
-This plugin is licensed under the [MIT License](http://opensource.org/licenses/MIT)
+[MIT License](http://opensource.org/licenses/MIT)
 
 ## Supported Browsers:
 Chrome, Safari, Opera, FireFox 17+, IE9+.
 
 ## Changelog:
+
+##### v.3.0.0 - 2014/10/14
+* Removed jQuery dependency
+* Added 'wheelzoom.destroy' event for removing changes that wheelzoom has made to an element.
 
 ##### v.2.0.1 - 2014/9/9
 * Merged bugfix to wrap background-image path in quotes.  Ref #8.
---
+++ wheelzoom.js
@@ -0,0 +1,195 @@
+/*!
+	Wheelzoom 3.0.0
+	license: MIT
+	http://www.jacklmoore.com/wheelzoom
+*/
+window.wheelzoom = (function(){
+	var defaults = {
+		zoom: 0.10
+	};
+	var canvas = document.createElement('canvas');
+
+	function setSrcToBackground(img) {
+		img.style.backgroundImage = "url('"+img.src+"')";
+		img.style.backgroundRepeat = 'no-repeat';
+		canvas.width = img.naturalWidth;
+		canvas.height = img.naturalHeight;
+		img.src = canvas.toDataURL();
+	}
+
+	main = function(img, options){
+		if (!img) { return; }
+
+		var settings = {};
+		var width;
+		var height;
+		var bgWidth;
+		var bgHeight;
+		var bgPosX;
+		var bgPosY;
+		var previousEvent;
+
+		function updateBgStyle() {
+			if (bgPosX > 0) {
+				bgPosX = 0;
+			} else if (bgPosX < width - bgWidth) {
+				bgPosX = width - bgWidth;
+			}
+
+			if (bgPosY > 0) {
+				bgPosY = 0;
+			} else if (bgPosY < height - bgHeight) {
+				bgPosY = height - bgHeight;
+			}
+
+			img.style.backgroundSize = bgWidth+'px '+bgHeight+'px';
+			img.style.backgroundPosition = bgPosX+'px '+bgPosY+'px';
+		}
+
+		function reset() {
+			bgWidth = width;
+			bgHeight = height;
+			bgPosX = bgPosY = 0;
+			updateBgStyle();
+		}
+
+		function onwheel(e) {
+			var deltaY = 0;
+
+			e.preventDefault();
+
+			if (e.deltaY) { // FireFox 17+ (IE9+, Chrome 31+?)
+				deltaY = e.deltaY;
+			} else if (e.wheelDelta) {
+				deltaY = -e.wheelDelta;
+			}
+
+			// As far as I know, there is no good cross-browser way to get the cursor position relative to the event target.
+			// We have to calculate the target element's position relative to the document, and subtrack that from the
+			// cursor's position relative to the document.
+			var rect = img.getBoundingClientRect();
+			var offsetX = e.pageX - rect.left - document.body.scrollLeft;
+			var offsetY = e.pageY - rect.top - document.body.scrollTop;
+
+			// Record the offset between the bg edge and cursor:
+			var bgCursorX = offsetX - bgPosX;
+			var bgCursorY = offsetY - bgPosY;
+			
+			// Use the previous offset to get the percent offset between the bg edge and cursor:
+			var bgRatioX = bgCursorX/bgWidth;
+			var bgRatioY = bgCursorY/bgHeight;
+
+			// Update the bg size:
+			if (deltaY < 0) {
+				bgWidth += bgWidth*settings.zoom;
+				bgHeight += bgHeight*settings.zoom;
+			} else {
+				bgWidth -= bgWidth*settings.zoom;
+				bgHeight -= bgHeight*settings.zoom;
+			}
+
+			// Take the percent offset and apply it to the new size:
+			bgPosX = offsetX - (bgWidth * bgRatioX);
+			bgPosY = offsetY - (bgHeight * bgRatioY);
+
+			// Prevent zooming out beyond the starting size
+			if (bgWidth <= width || bgHeight <= height) {
+				reset();
+			} else {
+				updateBgStyle();
+			}
+		}
+
+		function drag(e) {
+			e.preventDefault();
+			bgPosX += (e.pageX - previousEvent.pageX);
+			bgPosY += (e.pageY - previousEvent.pageY);
+			previousEvent = e;
+			updateBgStyle();
+		}
+
+		function removeDrag() {
+			document.removeEventListener('mouseup', removeDrag);
+			document.removeEventListener('mousemove', drag);
+		}
+
+		// Make the background draggable
+		function draggable(e) {
+			e.preventDefault();
+			previousEvent = e;
+			document.addEventListener('mousemove', drag);
+			document.addEventListener('mouseup', removeDrag);
+		}
+
+		function loaded() {
+			var computedStyle = window.getComputedStyle(img, null);
+
+			width = parseInt(computedStyle.width, 10);
+			height = parseInt(computedStyle.height, 10);
+			bgWidth = width;
+			bgHeight = height;
+			bgPosX = 0;
+			bgPosY = 0;
+
+			setSrcToBackground(img);
+
+			img.style.backgroundSize =  width+'px '+height+'px';
+			img.style.backgroundPosition = '0 0';
+			img.addEventListener('wheelzoom.reset', reset);
+
+			img.addEventListener('wheel', onwheel);
+			img.addEventListener('mousedown', draggable);
+		}
+
+		img.addEventListener('wheelzoom.destroy', function (originalProperties) {
+			console.log(originalProperties);
+			img.removeEventListener('wheelzoom.destroy');
+			img.removeEventListener('wheelzoom.reset', reset);
+			img.removeEventListener('load', onload);
+			img.removeEventListener('mouseup', removeDrag);
+			img.removeEventListener('mousemove', drag);
+			img.removeEventListener('mousedown', draggable);
+			img.removeEventListener('wheel', onwheel);
+
+			img.style.backgroundImage = originalProperties.backgroundImage;
+			img.style.backgroundRepeat = originalProperties.backgroundRepeat;
+			img.src = originalProperties.src;
+		}.bind(null, {
+			backgroundImage: img.style.backgroundImage,
+			backgroundRepeat: img.style.backgroundRepeat,
+			src: img.src
+		}));
+
+		options = options || {};
+
+		Object.keys(defaults).forEach(function(key){
+			settings[key] = options[key] !== undefined ? options[key] : defaults[key];
+		});
+
+		if (img.complete) {
+			loaded();
+		} else {
+			function onload() {
+				img.removeEventListener('load', onload);
+				loaded();
+			}
+			img.addEventListener('load', onload);
+		}
+	};
+
+	// Do nothing in IE8
+	if (typeof window.getComputedStyle !== 'function') {
+		return function(elements) {
+			return elements;
+		}
+	} else {
+		return function(elements, options) {
+			if (elements && elements.length) {
+				Array.prototype.forEach.call(elements, main, options);
+			} else if (elements && elements.nodeName) {
+				main(elements, options);
+			}
+			return elements;
+		}
+	}
+}());
